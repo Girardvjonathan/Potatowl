6413aa2c58ea05f2c62f6b392cb18a2cb7cedd93
Jonathan Girard Viau <girardvjonathan@gmail.com>
2017-02-27 16:36:21 -0500

    nothing to see

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-angular.module('app', ['ngRoute', 'appRoutes', 'MainCtrl', 'SeriesCtrl','SeriesService', 'SearchCtrl']);
+
+
+angular.module('app', ['ngRoute', 'appRoutes', 'MainCtrl', 'SeriesCtrl','SeriesService', 'SearchCtrl','stormpath', 'stormpath.templates'])
+.config(function (STORMPATH_CONFIG) {
+
+    // Specify your Client API domain here:
+
+    STORMPATH_CONFIG.ENDPOINT_PREFIX = 'https://husky-spear.id.stormpath.io';
+  });
+
