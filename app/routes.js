271d3c59c621edd2882d4259cb5a632e65280606
William Martel <willmart7@hotmail.com>
2017-03-13 16:56:48 -0400

    Moved serie information from Node to Angular

--- app/routes.js
+++ app/routes.js
@@ -1,37 +1,7 @@
-const KEY = '?api_key=1b1497adc03fb28cf8df7fa0cdaed980';
-const HOST = 'api.themoviedb.org';
-
 module.exports = function(app) {
 
 	// server routes ===========================================================
-	app.get('/api/series/:id', function(req, res) {
-		var http = require('http');
 
-		var options = {
-			host: HOST,
-			path: '/3/tv/' + req.params.id + KEY
-		};
-
-		callback = function(response) {
-			var data = '';
-
-			response.on('data', function (chunk) {
-				data += chunk;
-			});
-
-			response.on('end', function () {
-				try {
-					console.log(data);
-					res.json(data);
-				} catch (e) {
-					console.log('Error parsing JSON!');
-				}
-			});
-		}
-
-		http.request(options, callback).end();
-	});
-	
 	// authentication routes
 
 	// frontend routes =========================================================
